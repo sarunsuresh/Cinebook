9ddb6ef3b82ff49a2680cd1c40a79349dbc5fc00
sarunsuresh <sarunsuresh03@gmail.com>
2026-02-08 15:20:17 +0530

    feat: implement dynamic CORS using FRONTEND_URL

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ const authroute=require('./routes/auth')
 const adminRoute=require('./routes/admin')
 const userRoute=require('./routes/user')
 const app=express()
-const cors=require('cors')
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
 app.use(express.json())
 require('dotenv').config()
+const cors=require('cors')
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+  credentials: true
+}));
+
 const mongoose =require('mongoose')
 console.log(process.env.URL)
 mongoose.connect(process.env.URL).then(()=>{
